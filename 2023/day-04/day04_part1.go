a0d76ac37b2358abd4e76d951f00570ea585733c
Chris Baumann <christopher.baumann@konghq.com>
2023-12-04 10:44:57 +0100

    feat(2023): add day 4 solutions

---
+++ 2023/day-04/day04_part1.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	//Read input file
+	input, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	sc := bufio.NewScanner(input)
+
+	// Sum of all the winning card values
+	totalValue := 0
+
+	for sc.Scan() {
+		line := sc.Text()
+		split := strings.Split(line, ":")
+
+		game := split[1] // i.e " 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+		gameInfo := strings.Split(game, "|")
+		winningNumbers := parseNumbers(gameInfo[0]) // i.e [41, 48, 83, 86, 17]
+		cardNumbers := parseNumbers(gameInfo[1])    // i.e [83, 86,  6, 31, 17,  9, 48, 53]"
+
+		cardValue := calculateCardValue(winningNumbers, cardNumbers)
+
+		totalValue += cardValue
+	}
+
+	// Print the answer
+	fmt.Println(totalValue)
+
+	// Close Input
+	err = input.Close()
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func parseNumbers(s string) []int {
+	s = strings.TrimSpace(s)
+	values := strings.Split(s, " ")
+
+	var result []int
+
+	for _, value := range values {
+		if value != "" {
+			num, err := strconv.Atoi(value)
+			if err != nil {
+				panic(err)
+			}
+			result = append(result, num)
+		}
+	}
+
+	return result
+}
+
+func calculateCardValue(winningNumbers []int, cardNumbers []int) int {
+	var winners []int
+
+	for _, num := range cardNumbers {
+		if contains(winningNumbers, num) {
+			winners = append(winners, num)
+		}
+	}
+
+	if len(winners) == 0 {
+		return 0
+	} else {
+		return int(math.Pow(float64(2), float64(len(winners)-1)))
+	}
+
+}
+
+func contains(arr []int, val int) bool {
+	for _, v := range arr {
+		if v == val {
+			return true
+		}
+	}
+	return false
+}
---
+++ 2023/day-04/day04_part2.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Card struct {
+	copies int
+	value  string
+}
+
+func main() {
+	//Read input file
+	input, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	sc := bufio.NewScanner(input)
+
+	// Sum of all the winning card values
+	totalValue := 0
+
+	var cards []Card
+
+	for sc.Scan() {
+		line := sc.Text()
+
+		cards = append(cards, Card{1, line})
+	}
+
+	for pos, card := range cards {
+		split := strings.Split(card.value, ":")
+
+		game := split[1] // i.e " 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+		gameInfo := strings.Split(game, "|")
+		winningNumbers := parseNumbersToArray(gameInfo[0]) // i.e [41, 48, 83, 86, 17]
+		cardNumbers := parseNumbersToArray(gameInfo[1])    // i.e [83, 86,  6, 31, 17,  9, 48, 53]"
+
+		cardValue := calculateValue(winningNumbers, cardNumbers)
+
+		for i := 1; i <= cardValue; i++ {
+			cards[pos+i].copies += card.copies
+		}
+	}
+
+	for _, card := range cards {
+		totalValue += card.copies
+	}
+
+	// sum the total copies of all the cards
+
+	// Print the answer
+	fmt.Println(totalValue)
+
+	// Close Input
+	err = input.Close()
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func parseNumbersToArray(s string) []int {
+	s = strings.TrimSpace(s)
+	values := strings.Split(s, " ")
+
+	var result []int
+
+	for _, value := range values {
+		if value != "" {
+			num, err := strconv.Atoi(value)
+			if err != nil {
+				panic(err)
+			}
+			result = append(result, num)
+		}
+	}
+
+	return result
+}
+
+func calculateValue(winningNumbers []int, cardNumbers []int) int {
+	var winners []int
+
+	for _, num := range cardNumbers {
+		if containsInt(winningNumbers, num) {
+			winners = append(winners, num)
+		}
+	}
+
+	return len(winners)
+}
+
+func containsInt(arr []int, val int) bool {
+	for _, v := range arr {
+		if v == val {
+			return true
+		}
+	}
+	return false
+}
